06098c97ad67fb4d89bf46c13c9a15040dda5c08
Eder Gomez de Segura <ederaraia@gmail.com>
2018-01-31 17:43:03 +0100

    Ejercicio 12 new items Back-End

---
+++ BackEnd/12 News Items/src/resources/Messages_eu.java
@@ -0,0 +1,21 @@
+package resources;
+
+import java.util.ListResourceBundle;
+
+public class Messages_eu  extends ListResourceBundle {
+
+	@Override
+	protected Object[][] getContents() {
+		// TODO Auto-generated method stub
+		return contents;
+	}
+	
+	static final Object[][] contents = {
+			{"login.successful", "Saioa hasi duzu!"},
+			{"logout.successful", "Saioa itxi duzu!"},
+			{"newUser.successful", "Erabiltzailea erregistratu da!"},
+			{"editUser.successful", "Erabiltzailea eguneratu da."},
+			{"newNewsItem.successful", "Berria gorde da!"},
+			{"editNewItem.successful", "Berria eguneratu da."},
+	};
+}
