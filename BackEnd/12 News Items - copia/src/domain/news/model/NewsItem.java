cefc8037d4308bb67d2a0047f9bd59b4a1957fc5
Eder Gomez de Segura <ederaraia@gmail.com>
2018-01-31 17:43:37 +0100

    Ejercicio 12 new items copia Back-End

---
+++ BackEnd/12 News Items - copia/src/domain/news/model/NewsItem.java
@@ -0,0 +1,58 @@
+package domain.news.model;
+
+import java.util.Date;
+import java.util.Locale;
+
+import domain.user.model.User;
+
+public class NewsItem implements java.io.Serializable{
+	private static final long serialVersionUID = 7927744272163377158L;
+	private Integer newsItemId;
+	private String title;
+	private String body;
+	private User author;
+	private Date date;
+	private Locale lang;
+	
+	public NewsItem(){}
+	
+	public Integer getNewsItemId() {
+		return newsItemId;
+	}
+	public void setNewsItemId(Integer newsItemId) {
+		this.newsItemId = newsItemId;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+	public User getAuthor() {
+		return author;
+	}
+	public void setAuthor(User author) {
+		this.author = author;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public Locale getLang() {
+		return lang;
+	}
+	public void setLang(Locale lang) {
+		this.lang = lang;
+	}
+	
+	
+	
+}
